baed5a1bb7fa518db4868fa00676c761bbd71044
hemav <hemav@LAPTOP-BH7SCHNS>
2024-12-11 14:52:42 +0530

    first commit

--- BestIU_Files2/src/org/tns/capgemini/c2tc/streamapi/WithStreamAPI.java
+++ BestIU_Files2/src/org/tns/capgemini/c2tc/streamapi/WithStreamAPI.java
@@ -9,7 +9,7 @@ public class WithStreamAPI {
         List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
         List<Integer> result = numbers.stream() // Step 1: Create a stream from the list
                                       .filter(n -> n % 2 == 0)  // Intermediate Operation: Keep only even numbers
-                                      .map(n -> n * 2)          // Intermediate Operation: Double each number
+                                      .map(n -> n * 2)          // Intermediate Operation: MULTIPLY the even number with 2
                                       .sorted()                 // Intermediate Operation: Sort the numbers
                                       .collect(Collectors.toList()); // Terminal Operation: Collect results into
         //a list
